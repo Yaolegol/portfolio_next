f20678f7a1c90794b915c4d2dcd6e648ea7efc50
Yaolegol <m160160@yandex.ru>
2024-07-28 19:41:14 +0700

    Добавлены данные и стили

---
+++ src/modules/Home/data/about/about.ts
@@ -0,0 +1,94 @@
+export const data = [
+    {
+        'descriptionList': [
+            {
+                'description': 'about.city.value'
+            }
+        ],
+        'title': 'about.city.title'
+    },
+    {
+        'descriptionList': [
+            {
+                'description': 'about.education.value1'
+            },
+            {
+                'description': 'about.education.value2'
+            }
+        ],
+        'title': 'about.education.title'
+    },
+    {
+        'descriptionList': [
+            {
+                'description': 'about.workExperience.value1'
+            },
+            {
+                'description': 'about.workExperience.value2'
+            }
+        ],
+        'title': 'about.workExperience.title'
+    },
+    {
+        'descriptionList': [
+            {
+                'description': 'about.interests.value'
+            }
+        ],
+        'title': 'about.interests.title'
+    },
+    {
+        'descriptionList': [
+            {
+                'description': 'about.skills.value1',
+                'title': 'about.skills.value1Title'
+            },
+            {
+                'description': 'about.skills.value2',
+                'title': 'about.skills.value2Title'
+            },
+            {
+                'description': 'about.skills.value3',
+                'title': 'about.skills.value3Title'
+            },
+            {
+                'description': 'about.skills.value4',
+                'title': 'about.skills.value4Title'
+            },
+            {
+                'description': 'about.skills.value5',
+                'title': 'about.skills.value5Title'
+            }
+        ],
+        'title': 'about.skills.title'
+    },
+    {
+        'descriptionList': [
+            {
+                'description': 'about.experience.value1'
+            },
+            {
+                'description': 'about.experience.value2'
+            },
+            {
+                'description': 'about.experience.value3'
+            },
+            {
+                'description': 'about.experience.value4'
+            },
+            {
+                'description': 'about.experience.value5'
+            },
+            {
+                'description': 'about.experience.value6'
+            },
+            {
+                'description': 'about.experience.value7'
+            },
+            {
+                'description': 'about.experience.value8'
+            }
+        ],
+        'title': 'about.experience.title'
+    }
+];
---
+++ src/modules/Home/data/contacts.ts
@@ -0,0 +1,4 @@
+export const data = {
+    'email': 'web160160@gmail.com',
+    'phone': '+79069473139'
+};
