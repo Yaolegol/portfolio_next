d31e07626b65f14d07230874065558e45a5fa108
Yaolegol <m160160@yandex.ru>
2024-10-26 15:56:57 +0700

    Удален тестовый функционал

--- src/modules/Layout/index.tsx
+++ src/modules/Layout/index.tsx
@@ -1,10 +1,6 @@
 import { FC, ReactNode } from 'react';
 import style from './index.module.scss';
 import '@/icons/arrow.svg';
-import test from '@public/assets.json';
-
-console.log('test');
-console.log(test);
 
 interface IProps {
   children: ReactNode;
