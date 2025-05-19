978297d82d588b241cc73240fae5c7be77ad1d52
Yaolegol <m160160@yandex.ru>
2025-05-19 16:30:35 +0700

    Добавлена функция корректировки языка пользователя

--- src/helpers/lang/index.ts
+++ src/helpers/lang/index.ts
@@ -1,10 +1,19 @@
 import { LANG_OPTIONS } from '@/modules/Lang/constants';
 
+const getCorrectLang = (lang: string) => {
+    const isRu = lang.toLowerCase().includes('ru');
+
+    return isRu ? 'ru-RU' : 'en-US';
+};
+
 export const getLangText = async (lang: string) => {
+    const correctLang = getCorrectLang(lang);
     let langText = {};
 
     try {
-        const data = await import(`@/modules/Home/text/${lang}/index.json`);
+        const data = await import(
+            `@/modules/Home/text/${correctLang}/index.json`
+        );
 
         langText = data.default;
     } catch (e) {
@@ -15,5 +24,10 @@ export const getLangText = async (lang: string) => {
 };
 
 export const getLangOption = (lang: string) => {
-    return LANG_OPTIONS.find(({ value }) => value === lang) ?? LANG_OPTIONS[0];
+    const correctLang = getCorrectLang(lang);
+
+    return (
+        LANG_OPTIONS.find(({ value }) => value === correctLang) ??
+        LANG_OPTIONS[0]
+    );
 };
